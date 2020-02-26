1159c089068e213cd43857d25d05b406a3d2eba1
jeromescuggs <subspacer@gmail.com>
2020-02-26 12:00:53 -0600

    init

---
+++ test.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+
+# ======== [ hazardous code ] ========
+#  ███████╗ ██╗  ██╗ ██╗   ██╗ ██╗ shreveport's 
+#  ██╔════╝ ██║  ██║ ██║   ██║ ██║ helper for
+#  ███████╗ ███████║ ██║   ██║ ██║ verifying 
+#  ╚════██║ ██╔══██║ ╚██╗ ██╔╝ ██║ lstart time 
+#  ███████║ ██║  ██║  ╚████╔╝  ███████╗ 
+#  ╚══════╝ ╚═╝  ╚═╝   ╚═══╝   ╚══════╝
+# =======[ https://ha.zardo.us ]========
+
+
+
+
+# https://jonalmeida.com/posts/2013/05/26/different-ways-to-implement-flags-in-bash/
+
+
+# grab token from local environment
+SHVL_TOKEN=$(cat /home/$USER/.secrets/tbtoken-1)
+SHVL_TOKEN2=$(cat /home/$USER/.secrets/tbtoken-2)
+
+# setup variables 
+#SHVL_PH=
+#SHVL_MSG=
+
+# allow setting of vars with flags 
+while [ ! $# -eq 0 ]
+do 
+  case "$1" in 
+    --phone | -p)
+        SHVL_PH=$1
+        ;;
+      --msg | -m)
+        SHVL_MSG=$2
+        ;;
+  esac 
+  shift
+done
+
+echo "text ${SHVL_PH} with message ${SHVL_MSG}?"
+
+# execute API call to textbelt
+# curl -X POST https://textbelt.com/text \
+#         --data-urlencode phone=$SHVL_PH \
+#         --data-urlencode message=$SHVL_MSG \
+#          -d key=$SHVL_TOKEN
